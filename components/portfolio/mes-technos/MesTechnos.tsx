44145ef21e4368d386f3b633aa6d8902cb3a53d9
yannou64 <yannickpro64@gmail.com>
2025-11-14 10:59:31 +0100

    fix: error de compilation

--- components/portfolio/mes-technos/MesTechnos.tsx
+++ components/portfolio/mes-technos/MesTechnos.tsx
@@ -1,14 +1,27 @@
 import style from "./style.module.scss"
-import VignetteTechno from "./vignette-techno/VignetteTechno";
+// import VignetteTechno from "./vignette-techno/VignetteTechno";
 
-export default function MesTechnos({ technosByCategorie = [] }) {
+interface MesTechnos {
+  technosByCategorie: Array<categorieAndHisTechnos>;
+} 
+
+interface categorieAndHisTechnos {
+  categorie: string,
+  technos: Array<techno>
+}
+
+interface techno {
+  test: string
+}
+
+export default function MesTechnos() {
   return (
     <section className={style.MesTechnos}>
       <h2>Ma Stack</h2>
       <div className={style.container_vignetteTechno}>
-        {technosByCategorie.map((categorieAndHisTechnos, index) => (
+        {/* {technosByCategorie.map((categorieAndHisTechnos: categorieAndHisTechnos, index) => (
           <VignetteTechno key={index} categorie={categorieAndHisTechnos.categorie} listeTechnos={categorieAndHisTechnos.technos} />
-        ))}
+        ))} */}
       </div>
     </section>
   );
